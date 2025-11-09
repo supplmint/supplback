cc1e6bfb9ee3c59c4f7633e3e1a6ffb2ef47afce
raeraerae2211-ux <raeraerae2211@gmail.com>
2025-11-09 15:13:55 +0700

    Add URLSearchParams to globals

--- src/globals.d.ts
+++ src/globals.d.ts
@@ -6,6 +6,12 @@ declare global {
   var process: NodeJS.Process;
   var __dirname: string;
   var __filename: string;
+  
+  // URLSearchParams is available in Node.js 18+
+  var URLSearchParams: {
+    new (init?: string[][] | Record<string, string> | string | URLSearchParams): URLSearchParams;
+    prototype: URLSearchParams;
+  };
 }
 
 export {};
